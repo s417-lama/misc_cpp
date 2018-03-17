0b2316b33cd17f5ae9be92203fef27f1a6a9e3bc
slama <s417.lama@gmail.com>
2018-03-17 23:29:23 +0900

    add template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <string>
+#include <cstdlib>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> veci;
+
+int main() {
+    return 0;
+}
